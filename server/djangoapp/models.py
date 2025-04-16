202319cb0db7a7d81bb74f360e7ab59a3dd758e1
TrentREis <133795091+TrentREis@users.noreply.github.com>
2025-04-16 15:05:33 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,13 +1,8 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
 from django.utils.timezone import now
 from datetime import datetime
-
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-# Create your models here.
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -20,7 +15,6 @@ class CarMake(models.Model):
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     dealer_id = models.IntegerField(null=True, blank=True)
-
     name = models.CharField(max_length=100)
 
     CAR_TYPES = [
@@ -31,15 +25,18 @@ class CarModel(models.Model):
         ('TRUCK', 'Truck'),
     ]
 
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default='SUV'
+    )
     year = models.IntegerField(
-    default=datetime.now().year,
-    validators=[
-        MaxValueValidator(datetime.now().year),
-        MinValueValidator(1980)
-    ]
-)
+        default=datetime.now().year,
+        validators=[
+            MaxValueValidator(datetime.now().year),
+            MinValueValidator(1980)
+        ]
+    )
 
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.year})"
-
